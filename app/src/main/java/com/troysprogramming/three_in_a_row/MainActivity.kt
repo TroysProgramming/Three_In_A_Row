8343d146f75a8938d12b3b547da671c22adfdb95
Troy <troysprogramming@gmail.com>
2021-10-25 09:53:12 +1100

    Initial commit

---
+++ app/src/main/java/com/troysprogramming/three_in_a_row/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.troysprogramming.three_in_a_row
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
